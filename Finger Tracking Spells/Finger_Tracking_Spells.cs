272e2bea81a1b2677df3cb4e5a295fe708111ab8
Wyatt Putnam <putnamwt@dukes.jmu.edu>
2021-09-18 12:39:58 -0400

    PosingHand class contains hand data, so two spell icons can spawn at once.

--- Finger Tracking Spells/Finger_Tracking_Spells.cs
+++ Finger Tracking Spells/Finger_Tracking_Spells.cs
@@ -10,14 +10,12 @@ namespace Finger_Tracking_Spells
     {
         public float[][] handPoses;
         public string[] spellIdAtPose;
-        public float cooldownTime;
-        float timeOfSwap = Time.time;
+        public float cooldownBetweenSwapTime;
+        public float cooldownBetweenCastTime;
+        PosingHand[] hands = new PosingHand[] { new PosingHand(Side.Left), new PosingHand(Side.Right) };
         List<WheelMenu.Orb> list;
         WheelMenu icons;
-        WheelMenu.Orb orb;
 
-        float[] leftHand = new float[5];
-        float[] rightHand = new float[5];
         public override IEnumerator OnLoadCoroutine(Level level)
         {
             EventManager.onCreatureSpawn += EventManager_onCreatureSpawn;
@@ -44,7 +42,8 @@ namespace Finger_Tracking_Spells
                         }
                         Debug.Log(debugShit);
                     }
-                    Debug.Log(cooldownTime);
+                    Debug.Log(cooldownBetweenSwapTime);
+                    Debug.Log(cooldownBetweenCastTime);
                 }
                 catch (Exception e)
                 {
@@ -56,72 +55,46 @@ namespace Finger_Tracking_Spells
         public override void Update(Level level)
         {
             base.Update(level);
-            UpdateFingers();
-            if (PlayerControl.controller == PlayerControl.Controller.Index)
+            Creature creature = Player.currentCreature;
+            foreach (PosingHand hand in hands)
             {
-                Creature creature = Player.currentCreature;
+                hand.UpdateFingers();
+                if (hand.orb != null && Time.time - hand.timeOfSwap >= cooldownBetweenSwapTime)
                 {
-                    if (Time.time - timeOfSwap >= cooldownTime && orb != null)
+                    hand.DespawnOrb(hand.orb);
+                }
+                for (int i = 0; i < handPoses.Length; i++)
+                {
+                    if (hand.CheckForPose(handPoses[i]))
                     {
-                        DespawnOrb(orb);
-                        orb = null;
-                    }
-                    for (int i = 0; i < handPoses.Length; i++)
-                    {
-                        if (CheckForPose(leftHand, handPoses[i]))
+                        if (!creature.mana.GetCaster(hand.side).isFiring && Time.time - hand.timeOfCast >= cooldownBetweenCastTime)
                         {
-                            if (creature.handLeft.grabbedHandle == null && Time.time - timeOfSwap >= cooldownTime)
+                            if (creature.GetHand(hand.side).grabbedHandle == null && Time.time - hand.timeOfSwap >= cooldownBetweenSwapTime)
                             {
-                                LoadSpell(creature.mana.casterLeft, Side.Left, GetSpell(spellIdAtPose[i]));
-                                timeOfSwap = Time.time;
+                                LoadSpell(hand, creature.mana.GetCaster(hand.side), hand.side, GetSpell(spellIdAtPose[i]));
+                                hand.timeOfSwap = Time.time;
                             }
                         }
-                        if (CheckForPose(rightHand, handPoses[i]))
+                        else
                         {
-                            if (creature.handRight.grabbedHandle == null && Time.time - timeOfSwap >= cooldownTime)
-                            {
-                                LoadSpell(creature.mana.casterRight, Side.Right, GetSpell(spellIdAtPose[i]));
-                                timeOfSwap = Time.time;
-                            }
+                            hand.timeOfCast = Time.time;
                         }
                     }
                 }
             }
         }
 
-        bool CheckForPose(float[] hand, float[] pose)
-        {
-            for (int i = 0; i < hand.Length; i++)
-            {
-                if (pose[i] == 1f)
-                {
-                    if (hand[i] < 0.8f)
-                    {
-                        return false;
-                    }
-                }
-                if (pose[i] == 0f)
-                {
-                    if (hand[i] > 0.8f)
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        void LoadSpell(SpellCaster hand, Side side, CatalogData spell)
+        void LoadSpell(PosingHand hand, SpellCaster caster, Side side, CatalogData spell)
         {
             if (spell != null)
             {
-                if (hand.spellInstance != null && hand.spellInstance.hashId == spell.hashId)
+                if (caster.spellInstance != null && caster.spellInstance.hashId == spell.hashId)
                 {
                     return;
                 }
-                hand.LoadSpell((SpellCastData)spell);
+                caster.LoadSpell((SpellCastData)spell);
                 this.list = PopulateSpellMenu();
-                SpawnOrb(GetSpellOrb(list, GetSpellIconEffect(spell.id)), Player.currentCreature.GetHand(side));
+                hand.SpawnOrb(GetSpellOrb(list, GetSpellIconEffect(spell.id)), Player.currentCreature.GetHand(side));
                 PlayerControl.GetHand(side).HapticShort(1f);
                 Debug.Log(spell.ToString());
             }
@@ -168,41 +141,6 @@ namespace Finger_Tracking_Spells
             return null;
         }
 
-        void SpawnOrb(WheelMenu.Orb orb, RagdollHand hand)
-        {
-            if (orb == null)
-            {
-                Debug.LogError("Null Spell Icon!");
-                return;
-            }
-            orb.transform = new GameObject("SpellIcon").transform;
-            orb.transform.SetParent(hand.transform, false);
-            orb.transform.localPosition += new Vector3(-0.1f, 0.0f, 0.06f);
-            orb.transform.localEulerAngles = new Vector3(180f, orb.transform.localEulerAngles.y, 90f);
-            orb.effectInstance = orb.effectData.Spawn(orb.transform, true, Array.Empty<Type>());
-            orb.effectInstance.Play(0);
-            orb.effectInstance.SetIntensity(0f);
-            orb.effectInstance.SetLayer(5);
-            this.orb = orb;
-        }
-
-        void DespawnOrb(WheelMenu.Orb orb)
-        {
-            orb.effectInstance.End(false, -1f);
-            orb.effectInstance.onEffectFinished += OnEffectFinish;
-            UnityEngine.Object.Destroy(orb.transform.gameObject, 5f);
-            list.Clear();
-            List<Transform> debug = new List<Transform>();
-            for (int i = 0; i < Player.currentCreature.handLeft.transform.childCount; i++)
-            {
-                debug.Add(Player.currentCreature.handLeft.transform.GetChild(i));
-            }
-            foreach (Transform i in debug)
-            {
-                Debug.Log(i);
-            }
-        }
-
         List<WheelMenu.Orb> PopulateSpellMenu()
         {
             List<WheelMenu.Orb> list = new List<WheelMenu.Orb>();
@@ -216,26 +154,6 @@ namespace Finger_Tracking_Spells
                 }
             }
             return list;
-        }
-
-        void OnEffectFinish(EffectInstance effectInstance)
-        {
-            this.orb.effectInstance.Despawn();
-            this.orb.effectInstance.onEffectFinished -= OnEffectFinish;
-        }
-
-        void UpdateFingers()
-        {
-            rightHand[0] = PlayerControl.handRight.thumbCurl;
-            rightHand[1] = PlayerControl.handRight.indexCurl;
-            rightHand[2] = PlayerControl.handRight.middleCurl;
-            rightHand[3] = PlayerControl.handRight.ringCurl;
-            rightHand[4] = PlayerControl.handRight.littleCurl;
-            leftHand[0] = PlayerControl.handLeft.thumbCurl;
-            leftHand[1] = PlayerControl.handLeft.indexCurl;
-            leftHand[2] = PlayerControl.handLeft.middleCurl;
-            leftHand[3] = PlayerControl.handLeft.ringCurl;
-            leftHand[4] = PlayerControl.handLeft.littleCurl;
         }
     }
 }
---
+++ Finger Tracking Spells/PosingHand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ThunderRoad;
+using UnityEngine;
+
+namespace Finger_Tracking_Spells
+{
+    class PosingHand
+    {
+        float[] hand = new float[5];
+        public Side side;
+        public WheelMenu.Orb orb;
+        public float timeOfSwap = Time.time;
+        public float timeOfCast = Time.time;
+
+        public PosingHand(Side side)
+        {
+            this.side = side;
+            hand[0] = PlayerControl.GetHand(side).thumbCurl;
+            hand[1] = PlayerControl.GetHand(side).indexCurl;
+            hand[2] = PlayerControl.GetHand(side).middleCurl;
+            hand[3] = PlayerControl.GetHand(side).ringCurl;
+            hand[4] = PlayerControl.GetHand(side).littleCurl;
+        }
+
+        public void UpdateFingers()
+        {
+            hand[0] = PlayerControl.GetHand(this.side).thumbCurl;
+            hand[1] = PlayerControl.GetHand(this.side).indexCurl;
+            hand[2] = PlayerControl.GetHand(this.side).middleCurl;
+            hand[3] = PlayerControl.GetHand(this.side).ringCurl;
+            hand[4] = PlayerControl.GetHand(this.side).littleCurl;
+        }
+
+        public bool CheckForPose(float[] pose)
+        {
+            for (int i = 0; i < hand.Length; i++)
+            {
+                if (pose[i] == 1f)
+                {
+                    if (this.hand[i] < 0.8f)
+                    {
+                        return false;
+                    }
+                }
+                if (pose[i] == 0f)
+                {
+                    if (this.hand[i] > 0.8f)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void SpawnOrb(WheelMenu.Orb orb, RagdollHand hand)
+        {
+            if (orb == null)
+            {
+                Debug.LogError("Null Spell Icon!");
+                return;
+            }
+            orb.transform = new GameObject("SpellIcon").transform;
+            orb.transform.SetParent(hand.transform, false);
+            orb.transform.localPosition += new Vector3(-0.1f, 0.0f, 0.06f);
+            orb.transform.localEulerAngles = new Vector3(180f, orb.transform.localEulerAngles.y, 90f);
+            orb.effectInstance = orb.effectData.Spawn(orb.transform, true, Array.Empty<Type>());
+            orb.effectInstance.Play(0);
+            orb.effectInstance.SetIntensity(0f);
+            orb.effectInstance.SetLayer(5);
+            this.orb = orb;
+        }
+
+        public void DespawnOrb(WheelMenu.Orb orb)
+        {
+            orb.effectInstance.End(false, -1f);
+            UnityEngine.Object.Destroy(orb.transform.gameObject, 5f);
+            this.orb = null;
+        }
+    }
+}
